3903f513cc9e820ffe1bf1323080727de3b83867
Abysswalkerrr <arthurmsouza321@gmail.com>
2025-12-22 09:03:48 -0300

    versão 1.0.3

--- updater-core/src/main/java/com/estoquefx/updater/core/AppInfo.java
+++ updater-core/src/main/java/com/estoquefx/updater/core/AppInfo.java
@@ -4,7 +4,7 @@ public class AppInfo {
     public static final String NOME_APP =
             "SistemaEstoqueFX";
     public static final String VERSAO   =
-            "1.0.2";
+            "1.0.3";
     public static final String UPDATE_URL =
             "https://api.github.com/repos/Abysswalkerrr/estoque_releases/releases/latest";
 }
